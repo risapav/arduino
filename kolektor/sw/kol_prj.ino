a6c31476635c1519cf4af5e7cd76d01348f2075b
Pavol Risa <risapav@gmail.com>
2019-02-05 21:25:51 +0100

    dopisanie poznamok do algoritmu

--- kolektor/sw/kol_prj.ino
+++ kolektor/sw/kol_prj.ino
@@ -1,140 +1,147 @@
-    /*
-    * Arduino LCD Tutorial
-    *
-    * Crated by Dejan Nedelkovski,
-    * www.HowToMechatronics.com
-    * pokus palo peter
-    */
+/*
+* Arduino
+*
+* (c) Peter Riša 2019
+* 
+* riadenie ohrevu vody cez slnečný kolektor
+*/
 
-    #include <OneWire.h>
-    #include <DallasTemperature.h>
-    #include <LiquidCrystal.h> // includes the LiquidCrystal Library 
-    //cerpadlo
-    #define CERPADLO_CHOD 1
-    #define CERPADLO_STOP 0
-    #define TEPLOTA_MAX 45.0
+// knižnice pre komunikáciu s teplomermi Dallas DS1820
+#include <OneWire.h>
+#include <DallasTemperature.h>
+// knižnica pre zobrazovanie na LCD
+#include <LiquidCrystal.h>
 
-    //relre je na pine 3
-    int relayPin = 3; 
-    /* DS18S20 Temperature chip i/o */
-    #define MAX_DS1820_SENSORS 5
-    int num_temp = 0; //pocet teplomerov pripojenych
-      //teplota[0] = T1 -teplota kolektora
-      //teplota[1] = T2 -teplota v bojleri
-      //teplota[2] = T3 -teplota okolia
-    float teplota[MAX_DS1820_SENSORS];
+// konštanty pre riadenie čerpadla
+#define CERPADLO_CHOD 1
+#define CERPADLO_STOP 0
+
+// konštanty pre nadefinovanie displeja LCD display 1602A
+#define LCD_WIDTH 16
+#define LCD_HEIGHT 2
+
+// konštanty pre zapnutie ohrevu
+#define TEPLOTA_MAX 45.0
+#define TEPLOTA_VIAC 5.0
+#define TEPLOTA_MENEJ 3.0
+
+// koľko môže byť DS18S20 teplomerov pripojených
+#define MAX_DS1820_SENSORS 5
+// pole teplôt      
+// teplota[0] = T1 - teplota kolektora
+// teplota[1] = T2 - teplota v bojleri
+// teplota[2] = T3 - teplota okolia
+float teplota[MAX_DS1820_SENSORS]; 
+
+// definovanie vstupov a výstupov arduina
+int relayPin = 3; // relé je na pine 3
+int num_temp = 0; //pocet teplomerov pripojenych
+// vytvoření instance oneWireDS z knihovny OneWire
+OneWire  oneWireDS(13);  // teplomery sú na pine 13
+// vytvoření instance senzoryDS z knihovny DallasTemperature
+DallasTemperature senzoryDS(&oneWireDS);
+
+byte addr[MAX_DS1820_SENSORS][8];
+char buf[20];
+// definovanie rozhrania displeja (rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
+
+// na začiatok čerpadlo stojí
+int cerpadlo = CERPADLO_STOP;
     
-    // vytvoření instance oneWireDS z knihovny OneWire
-    OneWire  oneWireDS(13);  // on pin 13
-    // vytvoření instance senzoryDS z knihovny DallasTemperature
-    DallasTemperature senzoryDS(&oneWireDS);
-    
-    byte addr[MAX_DS1820_SENSORS][8];
-    char buf[20];
+// priprav všetko pred spustením regulácie
+void setup() { 
 
+    // najprv predpokladame, že čerpadlo nejde
+    cerpadlo = CERPADLO_STOP;
 
-    /* LCD display 1602A */
-    #define LCD_WIDTH 16
-    #define LCD_HEIGHT 2
-    LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7) 
+    // iniciacia rele modulu
+    pinMode(relayPin, OUTPUT);  //Set pin for output
+    digitalWrite(relayPin, HIGH);  // HIGH is off, LOW is on
 
-    
-    void setup() { 
-        lcd.begin(LCD_WIDTH, LCD_HEIGHT); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display } 
-        
-        //napis uvodny text
-        lcd.setCursor(0,0);
-        lcd.print("(@) Peter Risa");
-        lcd.setCursor(0,1);
-        lcd.print("Slnecny kolektor");
-        delay(3000); // 3 seconds delay 
+    // začni komunikovať s displejom
+    lcd.begin(LCD_WIDTH, LCD_HEIGHT); 
 
-        // zapnutí komunikace knihovny s teplotním cidlom
-        senzoryDS.begin();
-        
-        //zisti pocet teplomerov
-        lcd.clear(); // Clears the LCD screen
-        lcd.setCursor(0,0);
-        lcd.print("DS1820 Test");
+    //napis uvodny text
+    lcd.setCursor(0,0); // riadok hore
+    lcd.print("(c) Peter Risa");
+    lcd.setCursor(0,1); // riadok dole
+    lcd.print("Slnecny kolektor");
+    delay(3000); // 3 seconds delay 
 
-        byte i;
-        for(i=0;i<MAX_DS1820_SENSORS;i++)
-        {  
-          if (!oneWireDS.search(addr[i]))
-          { 
-            lcd.setCursor(0,1);
+    // začni komunikovať s teplomermi
+    senzoryDS.begin();
+
+    //zisti počet teplomerov
+    lcd.clear(); // Clears the LCD screen
+    lcd.setCursor(0,0); // riadok hore
+    lcd.print("DS1820 Test");
+
+    byte i;
+    for(i=0;i < MAX_DS1820_SENSORS;i++) {  
+        if (!oneWireDS.search(addr[i])) { 
+            lcd.setCursor(0,1); // riadok dole
             lcd.print("No more addresses.");
             oneWireDS.reset_search();
             delay(250);
             break;        
-            
-          }
-          num_temp = i+1;
-          sprintf(buf, "Pocet teplomerov: %d",num_temp);
-          lcd.setCursor(0,1);
-          lcd.print(buf);
-          delay(2000);
         }
-        lcd.setCursor(0,0);
-        lcd.print("Aktualny stav");
-        //iniciacia rele modulu
-        pinMode(relayPin, OUTPUT);  //Set pin for output
-        digitalWrite(relayPin, HIGH);  // HIGH is off, LOW is on
+        num_temp ++; // počet teplomerov zvýš o 1
+        sprintf(buf, "Pocet teplomerov: %d",num_temp);
+        lcd.setCursor(0,1); // riadok dole
+        lcd.print(buf);
+        delay(2000);
     }
-
+    lcd.setCursor(0,0); // riadok hore
+    lcd.print("Aktualny stav");
+}
    
-    void loop() { 
-/*      
-      lcd.setCursor(1,0); 
-      lcd.blink(); //Displays the blinking LCD cursor
-      delay(2000);
-      lcd.noBlink(); // Turns off the blinking LCD cursor
-      lcd.setCursor(1,0);
-      lcd.cursor(); // Displays an underscore (line) at the position to which the next character will be written 
-      delay(2000); 
-      lcd.noCursor(); // Hides the LCD cursor 
-      lcd.clear(); // Clears the LCD screen
-*/
-   // nacita vsetky teplomery
-      byte sensor;
-      senzoryDS.requestTemperatures();
-    // ulozi teploty do pola teplota
-      for (sensor=0;sensor<num_temp;sensor++)
-        {
-          teplota[sensor]=senzoryDS.getTempCByIndex(sensor);
-        }
-        
+// riadenie ohrevu
+void loop() { 
+    // načíta všetky teplomery
+    byte sensor;
+    senzoryDS.requestTemperatures();
 
-    // spusti cerpadlo po vyhodnoteni teplot
-      int cerpadlo=CERPADLO_STOP;
-    //  cerpadlo ma bezat ak
-    //  teploty su aspon 2
-      if(num_temp<2)
+    // uloží teploty do poľa teplota
+    for (sensor=0;sensor < num_temp;sensor++) {
+        teplota[sensor]=senzoryDS.getTempCByIndex(sensor);
+    }      
+
+    //  čerpadlo má bežať ak teploty sú aspoň 2
+    if(num_temp<2)
         return;
-    // ak teplota T0 je o 5.0 stupnov vacsia ako T1 spusti cerpadlo
-      if(teplota[0]>(teplota[1]+4.0))
+
+    // ak teplota T1 (teplota kolektora) je o TEPLOTA_VIAC väčšia
+    // ako T2 (teplota v bojleri), potom spusti čerpadlo
+    if(teplota[0] > (teplota[1] + TEPLOTA_VIAC))
         cerpadlo=CERPADLO_CHOD;
-    // ak teplota T1 je viac ako TEPLOTA_MAX stupnov zastav cerpadlo, aby sme neprehriali bojler
-      if(teplota[1]>40.0)
+
+    // ak teplota T1 (teplota kolektora) bez TEPLOTA_MENEJ je menšia 
+    // ako T2 (teplota v bojleri), potom vypni čerpadlo
+    if((teplota[0] - TEPLOTA_MENEJ) < teplota[1] )
         cerpadlo=CERPADLO_STOP;
 
-//teraz vypni alebo zapni cerpadlo    
-      if(cerpadlo==CERPADLO_CHOD)
-        {
-        digitalWrite(relayPin, LOW);
-        }
-      else
-        {
-        digitalWrite(relayPin, HIGH);
-        }
-      //zobraz teplotu na displej uloz ich do pola teplot
-      for (sensor=0;sensor<num_temp;sensor++)
-        {
-          lcd.setCursor(0,1);
-          sprintf(buf, "C: %d, T%d: ",cerpadlo, sensor+1);
-          lcd.print(buf);
-          lcd.print(teplota[sensor]);
-          delay(1000);
-        }
+    // ak teplota T2 (teplota v bojleri) je viac ako TEPLOTA_MAX, 
+    // zastav čerpadlo, aby sme neprehriali bojler
+    if(teplota[1] > TEPLOTA_MAX)
+        cerpadlo=CERPADLO_STOP;
 
+    //teraz skutočne vypni alebo zapni čerpadlo    
+    if(cerpadlo == CERPADLO_CHOD) {
+        // zapni čerpadlo
+        digitalWrite(relayPin, LOW);
+    } else {
+        // vypni čerpadlo
+        digitalWrite(relayPin, HIGH);
     }
+
+    //zobraz teplotu na displej uloz ich do pola teplot
+    for (sensor=0;sensor < num_temp;sensor++) {
+        lcd.setCursor(0,1);
+        sprintf(buf, "C: %d, T%d: ",cerpadlo, sensor+1);
+        lcd.print(buf);
+        lcd.print(teplota[sensor]);
+        delay(1000);
+    }
+
+}
